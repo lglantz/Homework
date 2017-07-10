bea0c5581472a185f8a9b3c5f86b9fc4070da657
lglantz <loganglantz@gmail.com>
2017-07-10 18:45:08 -0400

    HW6 UPDATES

--- HW6_citipix/js/main.js
+++ HW6_citipix/js/main.js
@@ -3,23 +3,26 @@ $('#submit-btn').click(function() {
   // Prevents the button press from submitting
   event.preventDefault();
   // Defines a var of the user input
-  var cityRaw = $('#city-type').val();
-  // Transforms the input into a standard format
-  var city = (cityRaw).toLowerCase();
+  var city = $('#city-type').val().toLowerCase();
   // Compares city to pre-set values
   if (city === 'nyc' || city === 'new york' || city === 'new york city') {
+    $('body').removeClass();
     $('body').addClass('nyc');
   }
   else if (city === 'san francisco' || city ===  'sf' || city ===  'bay area' || city === 'the bay area') {
+    $('body').removeClass();
     $('body').addClass('sf');
   }
   else if (city === 'los angeles' || city ===  'la' || city ===  'lax') {
+    $('body').removeClass();
     $('body').addClass('la');
   }
   else if (city === 'austin' || city ===  'atx') {
+    $('body').removeClass();
     $('body').addClass('austin');
   }
   else if (city === 'sydney' || city ===  'syd') {
+    $('body').removeClass();
     $('body').addClass('sydney');
   }
   // Resets the image to skyline if the input doesn't match a preset value
